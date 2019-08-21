48d2cdd218f75462fa1c5b24d2cbb5906bc53e33
halfdev <moneyclip0309@gmail.com>
2019-08-22 00:20:33 +0900

    oauth2.0 google yet

---
+++ src/main/java/com/example/oauth2/security/OauthTwoController.java
@@ -0,0 +1,29 @@
+package com.example.oauth2.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OauthTwoController {
+
+    @GetMapping("/")
+    public String getAuthoriztionMessage() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess() {
+        return "hello";
+    }
+
+    @GetMapping("/loginFailure")
+    public String loginFailure() {
+        return "loginFailure";
+    }
+
+}
